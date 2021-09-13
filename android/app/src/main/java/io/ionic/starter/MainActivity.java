023739e0101415385e4d76cfd6002782f8a59110
Eguiguren <antxon.eguiguren@dxc.com>
2021-09-13 08:23:59 +0200

    App Center SDK addded

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -7,6 +7,10 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,8 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+
+    // AppCenter SDK integration
+    AppCenter.start(getApplication(), "42f4296b-a23c-417e-a4a1-f99349a42d3b", Analytics.class, Crashes.class);
   }
 }
